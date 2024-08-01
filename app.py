1eecd179f37b4e5cfa47ac83a0ffcb8564b6569d
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2024-08-01 12:53:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import os
 os.environ["TORCH_CUDNN_SDPA_ENABLED"] = "1"
 import torch
-import numpy as 
+import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from PIL import Image, ImageFilter
